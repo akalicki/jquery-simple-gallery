d6dcd33db6547b30a9538ddb711f4cdd97636b98
Alex Kalicki <alexander.kalicki@gmail.com>
2013-07-10 15:08:22 -0400

    Began gallery image-loading code.

--- simple-gallery.js
+++ simple-gallery.js
@@ -1,12 +1,35 @@
 (function($) {
     $.fn.gallery = function(options) {
-        var defaults = {
         
-        }
-        var options = $.extend(defaults, options);
+        $.fn.gallery.defaults = {
+            target: "",
+            waitTime: 2,
+            restartOnEnd: true,
+            thumbExt: ""
+        };
+        var options = $.extend($.fn.gallery.defaults, options);
         
+        init(this);
         return this.each(function() {
-        
+            $(this).click(onClick);
         });
+        
+        function init(obj) {
+            $(options.target).css({
+                "background-repeat": "no-repeat",
+                "background-position": "center"
+            });
+            loadImg(obj, 0);
+        }
+        
+        function loadImg(obj, index) {
+            var url = obj.get(index).src;
+            $(options.target).css("background-image", "url("+url+")");
+        }
+        
+        function onClick() {
+            
+        }      
+        
     };
 })(jQuery);
