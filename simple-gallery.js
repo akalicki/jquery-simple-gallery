febbc2e19b0a58d2bd32e2c79d0281c1b236afb6
Alex Kalicki <alexander.kalicki@gmail.com>
2013-07-10 00:21:55 -0400

    Created skeleton of project repository.

---
+++ simple-gallery.js
@@ -0,0 +1,12 @@
+(function($) {
+    $.fn.gallery = function(options) {
+        var defaults = {
+        
+        }
+        var options = $.extend(defaults, options);
+        
+        return this.each(function() {
+        
+        });
+    };
+})(jQuery);
