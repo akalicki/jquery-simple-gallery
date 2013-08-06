3e50e9c6149005ea7860da78e83536799eb67a1b
Alex Kalicki <alexander.kalicki@gmail.com>
2013-08-07 01:25:36 +0200

    Added photo gallery to page.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+    $(".section-photos .display-image").gallery({
+        source: ".section-photos img",
+        selectClass: "shown",
+        waitTime: 7000
+    });
+});
